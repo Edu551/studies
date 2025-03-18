dd8152515a126bb9fb3c6f78b5a659323c0a5efd
edu551 <eduardogoncalves18@gmail.com>
2025-03-18 17:57:48 -0300

    let code

---
+++ Let_code_py/217_contains_duplicate.py
@@ -0,0 +1,30 @@
+class Solution:
+    def containsDuplicate_shorting(self, nums: list[int]) -> bool: # O(n log n)
+        nums.sort()
+        index = 0
+        while index < len(nums) - 1:
+            if nums[index] == nums[index + 1]:
+                    return True
+            index += 1
+        
+        return False
+    
+    def containsDuplicate_hash_map(self, nums: list[int]) -> bool: # O(n)
+        seen = {}
+        for num in nums:
+            if num in seen and seen[num] >= 1:
+                return True
+            seen[num] = seen.get(num, 0) + 1
+
+        return False
+    
+    def hasDuplicate(self, nums: list[int]) -> bool: # O(n)
+        return len(nums) != len(set(nums))
+        
+s = Solution()
+nums = [1,2,3,1]
+nums_2 = [1,2,3,5]
+# print(s.containsDuplicate_shorting(nums))
+# print(s.containsDuplicate_shorting(nums_2))
+print(s.containsDuplicate_hash_map(nums))
+print(s.containsDuplicate_hash_map(nums_2))
