dffcb3b49d595eecf07fb9b288fb40ce3717e42b
edu551 <eduardogoncalves18@gmail.com>
2025-04-01 16:00:56 -0300

    improving day 02 logic

--- AoC_py/Day_02/Day_02_02.py
+++ AoC_py/Day_02/Day_02_02.py
@@ -1,48 +1,37 @@
 safe_reports = 0
-index = None
 
 def compare(nums: list[int]) -> bool:
-    global index
     if len(nums) < 2:
         return True
 
-    add = None
+    increasing = None
 
     for i in range(len(nums) - 1):
         diff = nums[i + 1] - nums[i]
 
         if abs(diff) > 3 or diff == 0:
-            if index is None:
-                index = i
             return False
         
-        if add is None:
-            add = diff > 0
-        elif (diff > 0) != add:
-            if index is None:
-                index = i
+        if increasing is None:
+            increasing = diff > 0
+        elif (diff > 0) != increasing:
             return False
         
     return True
 
+def can_be_made_safe(nums: list[int]) -> bool:
+    for i in range(len(nums)):
+        new_nums = nums[:i] + nums[i+1:]
+        if compare(new_nums):
+            return True
+    return False
+
 with open("input_02.txt", "r", encoding="utf-8") as file:
     for line in file:
         list_int = [int(i) for i in line.split()]
-        comp = compare(list_int)
-
-        if not comp:
-            if index > 0:
-                comp = compare([n for idx, n in enumerate(list_int) if idx != index - 1])
-
-            if index >= 0 and not comp:
-                comp = compare([n for idx, n in enumerate(list_int) if idx != index])
-
-            if index < len(list_int) - 1 and not comp:
-                comp = compare([n for idx, n in enumerate(list_int) if idx != index + 1])
-
-        if comp:
+        
+        if compare(list_int) or can_be_made_safe(list_int):
             safe_reports += 1
-        index = None
 
 
 
