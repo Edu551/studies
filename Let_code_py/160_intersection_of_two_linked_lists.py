a538385d5cdb64433357b117611bb92d21706827
edu551 <eduardogoncalves18@gmail.com>
2025-04-01 22:25:59 -0300

    two pointers approach

--- Let_code_py/160_intersection_of_two_linked_lists.py
+++ Let_code_py/160_intersection_of_two_linked_lists.py
@@ -13,14 +13,14 @@ class Recursive_Solution:
     def traverse_linked_list(self, head: ListNode, hash_set: dict):
         if head is None:
             return
-        if id(head) not in hash_set:
-            hash_set[id(head)] = head
+        if head not in hash_set:
+            hash_set[head] = head
         self.traverse_linked_list(head.next, hash_set)
 
     def compare_dict(self, head: ListNode, hash_set: dict) -> ListNode | None:
         if head is None:
             return None
-        if id(head) in hash_set:
+        if head in hash_set:
             return head
         return self.compare_dict(head.next, hash_set)
     
@@ -33,13 +33,24 @@ class Iterative_Solution:
     
     def traverse_linked_list(self, head: ListNode, hash_set: dict):
         while head:
-            if id(head.val) not in hash_set:
-                hash_set[id(head)] = head
+            if head not in hash_set:
+                hash_set[head] = head
             head = head.next
     
     def compare_dict(self, head: ListNode, hash_set: dict) -> ListNode | None:
         while head:
-            if id(head) in hash_set:
+            if head in hash_set:
                 return head
             head = head.next
         return None
+    
+class Two_Pointers_Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode | None:
+        pointerA, pointerB = headA, headB
+
+        while pointerA != pointerB:
+            pointerA = pointerA.next if pointerA else headB
+            pointerB = pointerB.next if pointerB else headA
+        
+        return pointerA
+
